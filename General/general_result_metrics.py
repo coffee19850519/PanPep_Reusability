7997761429b06caca312c78b759581065b2a9fa6
Kai Liu <86090978+Karity@users.noreply.github.com>
2023-04-21 21:11:51 +0800

    Update and rename result_metrics.py to general_result_metrics.py

--- General/general_result_metrics.py
+++ General/general_result_metrics.py
@@ -1,7 +1,10 @@
 import pandas as pd
 from sklearn.metrics import roc_auc_score, precision_recall_curve, auc
 import os
+
 from utils import get_peptide_tcr, Project_path, Data_config, Data_output, Train_Round, FilePath, load_csv_like_file, Train_output_dir, Test_output_dir, Zero_test_data
+from result_metrics import Args
+from result_metrics import get_metrics as zero_metrics
 
 
 def get_peptide_tcr_result(src_csv: FilePath, PepColumnName: str = 'Peptide', CdrColumeName: str = 'Alpha CDR3',
@@ -52,17 +55,32 @@ def get_metrics(result_path, label_path, result_PepColumnName='Peptide', result_
 
 
 if __name__ == '__main__':
+    args = Args(f=True, zf=False, z=False)
     for index in range(Train_Round):
-        F_roc_list, F_pr_list = [], []
+        F_roc_list, F_pr_list, Z_F_roc_list, Z_F_pr_list, Z_roc_list, Z_pr_list = [], [], [], [], [], []
         for kf_time in range(Data_config['dataset']['current_fold'][0], Data_config['dataset']['current_fold'][1]):
-            F_result_path = pd.read_csv(os.path.join(os.path.abspath(""),
-                                                     'Round' + str(index + 1), 'kfold' + str(kf_time), Data_config['Train']['General']['Test_result_path'], 'KFold_' + str(kf_time) + '_test_general_result.csv'))
-            F_y_label_path = pd.read_csv(os.path.join(Project_path, Data_output, 'kfold' + str(kf_time), 'KFold_' + str(kf_time) + '_test_general_data.csv'))
-            F_roc, F_pr = get_metrics(F_result_path, F_y_label_path)
-            F_roc_list.append(F_roc)
-            F_pr_list.append(F_pr)
-            print('Round:', index, 'K:', kf_time, '--General: ROC-AUC', F_roc, 'PR-AUC', F_pr)
-
-        mean_F_roc = sum(F_roc_list) / len(F_roc_list)
-        mean_F_pr = sum(F_pr_list) / len(F_pr_list)
-        print('Round:', index, 'average', '\n--General: ROC-AUC', mean_F_roc, 'PR-AUC', mean_F_pr)
+            if args.f:
+                F_result_path = pd.read_csv(os.path.join(os.path.abspath(""), Data_config['Train']['General']['Train_output_dir'],
+                                                         'Round' + str(index + 1), 'kfold' + str(kf_time), Test_output_dir, 'KFold_' + str(kf_time) + '_test_general_result.csv'))
+                F_y_label_path = pd.read_csv(os.path.join(Project_path, Data_output, 'kfold' + str(kf_time), 'KFold_' + str(kf_time) + '_test_general_data.csv'))
+                F_roc, F_pr = get_metrics(F_result_path, F_y_label_path)
+                F_roc_list.append(F_roc)
+                F_pr_list.append(F_pr)
+                print('Round:', index + 1, 'K:', kf_time, '--General: ROC-AUC', F_roc, 'PR-AUC', F_pr)
+            if args.z:
+                # zero-shot result metrics
+                Z_result_path = pd.read_csv(os.path.join(os.path.abspath(""), Data_config['Train']['General']['Train_output_dir'], 'Round' + str(index + 1),
+                                                         'kfold' + str(kf_time), Test_output_dir, 'Zero-shot_dataset_Result_Round_' + str(index + 1) + '_kfold_' + str(kf_time) + '_test.csv'))
+                Z_y_label_path = pd.read_csv(os.path.join(Project_path, Zero_test_data))
+                Z_roc, Z_pr = zero_metrics(Z_result_path, Z_y_label_path)
+                Z_roc_list.append(Z_roc)
+                Z_pr_list.append(Z_pr)
+                print('Round:', index + 1, 'K:', kf_time, '--Zero-shot: ROC-AUC', Z_roc, 'PR-AUC', Z_pr)
+        if args.f:
+            mean_F_roc = sum(F_roc_list) / len(F_roc_list)
+            mean_F_pr = sum(F_pr_list) / len(F_pr_list)
+            print('Round:', index + 1, 'average', '\n--General: ROC-AUC', mean_F_roc, 'PR-AUC', mean_F_pr)
+        if args.z:
+            mean_Z_roc = sum(Z_roc_list) / len(Z_roc_list)
+            mean_Z_pr = sum(Z_pr_list) / len(Z_pr_list)
+            print('Round:', index + 1, 'average', '\n--Zero-shot: ROC-AUC', mean_Z_roc, 'PR-AUC', mean_Z_pr)
