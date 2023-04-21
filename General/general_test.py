15a623e5ff3b9c80cc1d7de24eea1d1bfb260350
Kai Liu <86090978+Karity@users.noreply.github.com>
2023-04-21 21:10:43 +0800

    Update general_test.py

--- General/general_test.py
+++ General/general_test.py
@@ -1,15 +1,13 @@
-import argparse
 import os
-import numpy as np
 import pandas as pd
 import torch
-from sklearn.utils import shuffle
 from General_Model import Memory_Meta
 from collections import Counter
 import joblib
 
 
-from utils import Data_config, Project_path, Data_output, Device, Model_config, add_position_encoding, aamapping, Aa_dict, Train_Round
+from utils import Data_config, Project_path, Data_output, Device, Model_config, add_position_encoding, aamapping, Aa_dict, Train_Round, get_peptide_tcr, Test_output_dir
+from test_5fold import change_dict2test_struct
 
 
 def task_embedding(pep, tcr_data, aa_dict):
@@ -24,8 +22,6 @@ def task_embedding(pep, tcr_data, aa_dict):
     Returns:
         this function returns a peptide embedding and the embedding of query TCRs
     """
-
-    # Obtain the TCRs of support set
     spt_TCRs = tcr_data
 
     # Initialize the size of the Tensor for the query set and peptide encoding
@@ -59,29 +55,37 @@ for index in range(Train_Round):
             print(e)
             print("Not find test file")
             continue
-        peptides = test_data['peptide']
-        TCRs = test_data['binding_TCR']
-        labels = test_data['label']
-        F_data = {}
-        for i, j in enumerate(peptides):
-            if j not in F_data:
-                F_data[j] = [[], []]
-            if labels[i] != 'Unknown':
-                F_data[j][0].append(TCRs[i])
-                F_data[j][1].append(labels[i])
-        model_path = os.path.join(os.path.abspath(""), 'Round' + str(index + 1), 'kfold' + str(kf_time), 'KFold_' + str(kf_time) + '_best.pt')
+
+        # peptides = test_data['peptide']
+        # TCRs = test_data['binding_TCR']
+        # labels = test_data['label']
+        # F_data = {}
+        # for i, j in enumerate(peptides):
+        #     if j not in F_data:
+        #         F_data[j] = [[], []]
+        #     if labels[i] != 'Unknown':
+        #         F_data[j][0].append(TCRs[i])
+        #         F_data[j][1].append(labels[i])
+
+        F_data = change_dict2test_struct(get_peptide_tcr(test_data, 'peptide', 'binding_TCR', 'label'), None,
+                                         k_shot=2, k_query=3, lower_limit=None, upper_limit=None, ratio=1, has_nega=True)
+        Peptide = [key for key in F_data.keys() for i in range(6)]  # positive and negative total 6
+        CDR3 = []
+        [CDR3.extend(i) for i in [F_data[key][2] for key in F_data.keys()]]
+
+        model_path = os.path.join(os.path.abspath(""), Data_config['Train']['General']['Train_output_dir'], 'Round' + str(index + 1), 'kfold' + str(kf_time), 'KFold_' + str(kf_time) + '_best.pt')
         model.load_state_dict(torch.load(model_path))
         ends = []
         for i in F_data:
             # Convert the input into the embeddings
-            peptide_embedding, x_spt = task_embedding(i, F_data[i][0], aa_dict)
+            peptide_embedding, x_qry = task_embedding(i, F_data[i][2], aa_dict)
             # Support set is used for fine-tune the model and the query set is used to test the performance
-            end = model.test(x_spt[0].to(Device))
+            end = model.test(x_qry[0].to(Device))
             ends += list(end)
 
         # Store the predicted result and output the result as .csv file
-        output = pd.DataFrame({'Peptide': peptides, 'CDR3': TCRs, 'Score': ends})
-        output_file = os.path.join(os.path.abspath(""), 'Round' + str(index + 1), 'kfold' + str(kf_time), Data_config['Train']['General']['Test_result_path'])
+        output = pd.DataFrame({'Peptide': list(Peptide), 'CDR3': CDR3, 'Score': ends})
+        output_file = os.path.join(os.path.abspath(""), Data_config['Train']['General']['Train_output_dir'], 'Round' + str(index + 1), 'kfold' + str(kf_time), Test_output_dir)
         if not os.path.exists(output_file):
             os.makedirs(output_file)
         output.to_csv(os.path.join(output_file, 'KFold_' + str(kf_time) + '_test_general_result.csv'), index=False)
